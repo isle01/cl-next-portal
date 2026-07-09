97b59b192dc785c28b4371da357c4140d2c86c37
unknown <luv9821@gmail.com>
2026-07-10 00:12:13 +0900

    next.config.tsの変更

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,9 @@ const nextConfig: NextConfig = {
   /* config options here */
 };
 
-export default nextConfig;
+// 本番環境（デプロイ後）では実行しないようにガードするよ
+if (process.env.NODE_ENV === 'development') {
+  import('@opennextjs/cloudflare').then(m => m.initOpenNextCloudflareForDev());
+}
 
-import('@opennextjs/cloudflare').then(m => m.initOpenNextCloudflareForDev());
+export default nextConfig;
